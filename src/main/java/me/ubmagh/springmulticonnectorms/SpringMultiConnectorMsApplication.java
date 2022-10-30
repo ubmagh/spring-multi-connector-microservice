94837972b781fee493abbbfb3af3bb9507292d04
Ayoub👾🦆 <ubmagh@gmail.com>
2022-10-30 23:23:55 +0100

    �Initialize project

---
+++ src/main/java/me/ubmagh/springmulticonnectorms/SpringMultiConnectorMsApplication.java
@@ -0,0 +1,13 @@
+package me.ubmagh.springmulticonnectorms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultiConnectorMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMultiConnectorMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ubmagh/springmulticonnectorms/SpringMultiConnectorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ubmagh.springmulticonnectorms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultiConnectorMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
